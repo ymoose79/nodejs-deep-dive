f6bf9d5acd5ba1919c4cfa7236567de6bd3b20ae
ymoose79 <justin.r.stock@icloud.com>
2022-05-07 08:36:02 -0400

    delete checks file when user deletes acct

--- index.js
+++ index.js
@@ -103,6 +103,6 @@ const unifiedServer = function (req, res) {
 const router = {
   ping: handlers.ping,
   users: handlers.users,
-  tokens: handlers.tokens,
+  checks: handlers.checks,
   tokens: handlers.tokens
 };
